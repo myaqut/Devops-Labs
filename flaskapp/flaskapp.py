917f796714815c19e803a0cb276c21a0ce446f22
Mohamed YaQoot <mohamedyakut0@gmail.com>
2021-09-16 09:48:36 -0400

    Update flaskapp.py

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 
 def current_time():
+  #get timezone 
   country_time_zone = pytz.timezone('Europe/Moscow')
+  #get the time and date using the timezone
   country_time = datetime.now(country_time_zone)
+  #preparing the text to send to as a result
   result = "mosocow " + country_time.strftime("Date is %d-%m-%y and time is %H:%M:%S")
   return result
 
