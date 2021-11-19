bf80bdac0a50aea584a90d7a7bda0400bcc48c7c
yaQot <73448465+myaqut@users.noreply.github.com>
2021-11-19 18:04:34 +0200

    update terraform file

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  
+  backend "remote" {
+    organization = "yaqot"
+
+    workspaces {
+      name = "ec2"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0629230e074c580f2"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
